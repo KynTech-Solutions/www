82acbb5d23eaf2751919530f8b8ee539c8966c81
LuanCKRG <luancamposck@gmail.com>
2025-04-24 19:10:14 -0300

    style(header): add styles to kyntech "logo"

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export const Header = () => (
 	<header className="w-full flex justify-between items-center sticky top-0 z-50 backdrop-blur px-8 py-4 border-b border-border bg-background dark:bg-background">
-		<h1 className="">Kyntech</h1>
+		<h1 className="font-thin text-2xl uppercase">Kyntech</h1>
 		<nav className="flex gap-4">
 			<ul className="flex gap-x-2">
 				<li>
