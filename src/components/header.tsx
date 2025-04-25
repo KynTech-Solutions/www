fdb821831c30cd32eb51f4df56871536c0eac142
LuanCKRG <luancamposck@gmail.com>
2025-04-25 18:55:23 -0300

    refactor(header): simplify link rendering by using a links array

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,25 @@
 import { ToggleModeButton } from "@/components/toggle-mode-button"
 import { Button } from "@/components/ui/button"
+const linksArray = [
+	{ text: "Como funciona", href: "como-funciona" },
+	{ text: "Soluções", href: "solucoes" },
+	{ text: "Preços", href: "precos" },
+	{ text: "Contato", href: "contato" },
+	{ text: "FAQ", href: "faq" }
+]
 
 export const Header = () => (
 	<header className="w-full flex justify-between items-center sticky top-0 z-50 backdrop-blur px-8 py-4 border-b border-border bg-background dark:bg-background">
 		<h1 className="font-thin text-2xl uppercase">Kyntech</h1>
 		<nav className="flex gap-4">
 			<ul className="flex gap-x-2">
-				<li>
+				{linksArray.map(({ text, href }) => (
+					<li key={text}>
 					<Button variant="link" asChild>
-						<a href="#como-funciona">Como funciona</a>
+							<Link href={`#${href}`}>{text}</Link>
 					</Button>
 				</li>
-				<li>
-					<Button variant="link" asChild>
-						<a href="#solucoes">Soluções</a>
-					</Button>
-				</li>
-				<li>
-					<Button variant="link" asChild>
-						<a href="#precos">Preços</a>
-					</Button>
-				</li>
-				<li>
-					<Button variant="link" asChild>
-						<a href="#contato">Contato</a>
-					</Button>
-				</li>
-				<li>
-					<Button variant="link" asChild>
-						<a href="#faq">FAQ</a>
-					</Button>
-				</li>
+				))}
 			</ul>
 			<ToggleModeButton />
 		</nav>
