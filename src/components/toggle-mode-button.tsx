eae72bef8803d1c1370ad3df358b57adfd216a39
LuanCKRG <luancamposck@gmail.com>
2025-04-25 19:06:51 -0300

    feat(toggle-mode-button): pass props to Button component for better customization

--- src/components/toggle-mode-button.tsx
+++ src/components/toggle-mode-button.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
 
-export const ToggleModeButton = () => {
+export const ToggleModeButton = (props: React.ComponentProps<typeof Button>) => {
 	const { setTheme, theme } = useTheme()
 
 	function handleToggleTheme() {
@@ -17,7 +17,7 @@ export const ToggleModeButton = () => {
 	}
 
 	return (
-		<Button variant="outline" size="icon" onClick={handleToggleTheme}>
+		<Button variant="outline" size="icon" onClick={handleToggleTheme} {...props}>
 			<Sun className="size-6 lg:size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 			<Moon className="absolute size-6 lg:size-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
