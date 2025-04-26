3ec3c3b6ca5fe04d0bbb94793186ce0210b71201
LuanCKRG <luancamposck@gmail.com>
2025-04-26 18:09:30 -0300

    feat(header): adjust layout structure for improved responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,47 +14,49 @@ const linksArray = [
 ]
 
 export const Header = () => (
-	<header className="w-full flex justify-between items-center sticky top-0 z-50 backdrop-blur lg:px-8 py-4 border-b border-border bg-background dark:bg-background">
-		<Drawer>
-			<DrawerTrigger asChild>
-				<Button variant="ghost" size="icon" className="lg:hidden">
-					<AlignLeft className="size-6" />
-				</Button>
-			</DrawerTrigger>
+	<header className="w-full sticky top-0 z-50 backdrop-blur border-b border-border bg-background dark:bg-background">
+		<div className="container mx-auto px-4 lg:px-8 py-4 flex justify-between items-center">
+			<Drawer>
+				<DrawerTrigger asChild>
+					<Button variant="ghost" size="icon" className="lg:hidden">
+						<AlignLeft className="size-6" />
+					</Button>
+				</DrawerTrigger>
 
-			<DrawerContent className="pb-4">
-				<DrawerHeader>
-					<DrawerTitle className="sr-only">Links Úteis</DrawerTitle>
-				</DrawerHeader>
-				<nav>
-					<ul className="flex flex-col gap-y-2">
-						{linksArray.map(({ text, href }) => (
-							<li key={text}>
-								<Button variant="link" className="ml-3 text-xl" asChild>
-									<Link href={`#${href}`}>{text}</Link>
-								</Button>
-							</li>
-						))}
-					</ul>
-				</nav>
-			</DrawerContent>
-		</Drawer>
+				<DrawerContent className="pb-4 lg:hidden">
+					<DrawerHeader>
+						<DrawerTitle className="sr-only">Links Úteis</DrawerTitle>
+					</DrawerHeader>
+					<nav>
+						<ul className="flex flex-col gap-y-2">
+							{linksArray.map(({ text, href }) => (
+								<li key={text}>
+									<Button variant="link" className="ml-3 text-xl" asChild>
+										<Link href={`#${href}`}>{text}</Link>
+									</Button>
+								</li>
+							))}
+						</ul>
+					</nav>
+				</DrawerContent>
+			</Drawer>
 
-		<h1 className="font-thin text-2xl uppercase tracking-widest">Kyntech</h1>
+			<h1 className="font-thin text-2xl uppercase tracking-widest">Kyntech</h1>
 
-		<ToggleModeButton variant="ghost" className="lg:hidden" />
+			<ToggleModeButton variant="ghost" className="lg:hidden" />
 
-		<nav className="hidden lg:flex gap-4">
-			<ul className="flex gap-x-2">
-				{linksArray.map(({ text, href }) => (
-					<li key={text}>
-						<Button variant="link" asChild>
-							<Link href={`#${href}`}>{text}</Link>
-						</Button>
-					</li>
-				))}
-			</ul>
-			<ToggleModeButton />
-		</nav>
+			<nav className="hidden lg:flex gap-4">
+				<ul className="flex gap-x-2">
+					{linksArray.map(({ text, href }) => (
+						<li key={text}>
+							<Button variant="link" asChild>
+								<Link href={`#${href}`}>{text}</Link>
+							</Button>
+						</li>
+					))}
+				</ul>
+				<ToggleModeButton />
+			</nav>
+		</div>
 	</header>
 )
