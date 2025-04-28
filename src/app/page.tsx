7603edcc2c7aaf7218a92922dda6ea862e91ebf4
LuanCKRG <luancamposck@gmail.com>
2025-04-28 17:55:44 -0300

    feat(page): add SectionOne and SectionTwo components to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { Hero } from "@/components/hero"
+import { SectionOne } from "@/components/sections/section-one"
+import { SectionTwo } from "@/components/sections/section-two"
 
 const Home = () => {
 	return (
 		<>
 			<Hero />
+
+			<div className="space-y-20">
+				<SectionOne />
+				<SectionTwo />
+			</div>
 		</>
 	)
 }
