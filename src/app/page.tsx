41aee74521b45593fa0bf68797701e5497767518
LuanCKRG <luancamposck@gmail.com>
2025-05-01 19:23:59 -0300

    feat(page): import and integrate FAQ component into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/hero"
 import { SectionOne } from "@/components/sections/section-one"
 import { SectionTwo } from "@/components/sections/section-two"
+import { FAQ } from "@/components/faq"
 
 const Home = () => {
 	return (
@@ -9,7 +10,10 @@ const Home = () => {
 
 			<div className="space-y-20">
 				<SectionOne />
+
 				<SectionTwo />
+
+				<FAQ />
 			</div>
 		</>
 	)
