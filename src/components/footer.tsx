36b6dc7252c39a6474d7dd93f25820183c72e959
LuanCKRG <luancamposck@gmail.com>
2025-05-03 23:59:28 -0300

    feat(footer): add Footer component with styling and content

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => (
+	<footer className="border-t-[1px] border-border px-4 md:px-6 pt-10 md:pt-16 bg-[#fff] dark:bg-[#0C0C0C] overflow-hidden md:max-h-[400px]">
+		<div className="container">
+			<div className="flex flex-col border-border border-b-[1px] pb-10 md:pb-16 mb-12">
+				<span className="font-thin uppercase text-2xl md:text-4xl text-center text-white tracking-widest">Kyntech</span>
+				<span className="font-normal md:text-2xl text-center text-[#878787]">Transformando ideias em grandes negócios.</span>
+			</div>
+		</div>
+
+		<h5 className="dark:text-[#161616] text-[#F4F4F3] text-[280px] leading-none text-center pointer-events-none uppercase">Kyntech</h5>
+	</footer>
+)
