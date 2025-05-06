c4116da1e064be5627e5f7f7d07740f7bd2e43e1
LuanCKRG <luancamposck@gmail.com>
2025-05-06 19:42:12 -0300

    style(footer): update text colors for light theme

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,14 @@ export const Footer = () => (
 	<footer className="border-t-[1px] border-border px-4 md:px-6 pt-10 md:pt-16 bg-[#fff] dark:bg-[#0C0C0C] overflow-hidden md:max-h-[400px]">
 		<div className="container">
 			<div className="flex flex-col border-border border-b-[1px] pb-10 md:pb-16 mb-12">
-				<span className="font-thin uppercase text-2xl md:text-4xl text-center text-white tracking-widest">Kyntech</span>
-				<span className="font-normal md:text-2xl text-center text-[#878787]">Transformando ideias em grandes negócios.</span>
+				<h2 className="font-extralight dark:font-thin uppercase text-2xl md:text-4xl text-center text-black dark:text-white tracking-widest">Kyntech</h2>
+				<p className="font-light md:text-2xl text-center text-[#878787]">
+					Transformando <span className="text-black dark:text-white underline font-normal">ideias</span> em{" "}
+					<span className="text-black dark:text-white underline font-normal">grandes negócios</span>.
+				</p>
 			</div>
 		</div>
 
-		<h5 className="dark:text-[#161616] text-[#F4F4F3] text-[280px] leading-none text-center pointer-events-none uppercase">Kyntech</h5>
+		<p className="dark:text-[#161616] text-neutral-200 text-[280px] leading-none text-center pointer-events-none uppercase">Kyntech</p>
 	</footer>
 )
