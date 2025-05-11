f98413ddcce85d2b6b150b648c024a6a2069ebe3
LuanCKRG <luancamposck@gmail.com>
2025-05-11 00:09:38 -0300

    style(contact-section): update text colors for improved readability on light mode

--- src/components/sections/contact-section.tsx
+++ src/components/sections/contact-section.tsx
@@ -6,19 +6,19 @@ export const ContactSection = () => (
 		<SectionHeader className="mb-6 text-center">
 			<SectionTitle className="font-semibold text-xl sm:text-3xl tracking-tighter leading-7 sm:mb-4">Solicite sua proposta</SectionTitle>
 
-			<SectionDescription className="text-[#878787] text-center sm:text-lg">
-				Preencha os campos abaixo e um especialista entrará em contato em até <span className="text-white font-semibold">2 horas</span>.
+			<SectionDescription className="text-neutral-500 dark:text-neutral-300 text-center sm:text-lg">
+				Preencha os campos abaixo e um especialista entrará em contato em até <span className="text-black dark:text-white font-semibold">2 horas</span>.
 			</SectionDescription>
 		</SectionHeader>
 
-		<SectionContent className="md:grid md:grid-cols-2 md:gap-x-8 border border-border px-4 py-6 md:px-8  bg-[#F2F1EF] dark:bg-[#121212]">
+		<SectionContent className="md:grid md:grid-cols-2 md:gap-x-8 border border-border px-4 py-6 md:px-8 bg-foreground/10 dark:bg-[#121212]">
 			<ContactForm />
 
 			<div className="hidden md:flex flex-col justify-center items-center">
 				<h2 className="text-2xl text-center font-medium">Preencha os campos</h2>
-				<span className="text-center text-base text-[#878787]">
-					e receba <span className="text-white">hoje mesmo</span> um plano de desenvolvimento
-					<span className="text-white"> 100% personalizado</span> para sua ideia.
+				<span className="text-center text-base text-foreground/60 dark:text-[#878787]">
+					e receba <span className="text-black dark:text-white">hoje mesmo</span> um plano de desenvolvimento
+					<span className="text-black dark:text-white"> 100% personalizado</span> para sua ideia.
 				</span>
 			</div>
 		</SectionContent>
